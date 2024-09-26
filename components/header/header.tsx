e2b9f262844db9aaab3062e71cf7e3d625994eec
Vítor Johansen Guerra <vitorjguerra@hotmail.com>
2024-09-26 19:34:50 -0300

    add: links

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '@/components/header/header.module.scss'
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
     return (
@@ -8,9 +9,10 @@ export default function Header() {
             <nav className={styles.header}>
                 <Image className={styles.mainlogo} src="/images/logo.png" alt="Logo da Toca do Drag" width={100} height={91} />
                 <ul className={styles.headerMenu}>
-                    <li><a href="#home">Inicio</a></li>
-                    <li><a href="#about">Sobre</a></li>
-                    <li><a href="#guides">Participe</a></li>
+                    <li><Link href="#home">Inicio</Link></li>
+                    <li><Link href="#about">Sobre</Link></li>
+                    <li><Link href="#join">Participe</Link></li>
+                    <li><Link href="#https://discord.gg/vxwBY8E">Discord</Link></li>
                 </ul>
             </nav>
         </header>
